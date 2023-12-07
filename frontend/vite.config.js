fc13251e79857984790b71cbffd6eec11b4997ed
Joseph dat Dev <139043824+josephDeb@users.noreply.github.com>
2023-12-06 23:40:33 -0300

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://shopbhow-backend.onrender.com/",
+        target: "http://localhost:8088/",
         secure: false,
       }
     }
